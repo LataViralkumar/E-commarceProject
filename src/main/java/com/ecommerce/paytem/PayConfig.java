493476c2efea9a1a5c10a4b6ce77f6b39cbf3f19
LataViralkumar <lataviral520@gmail.com>
2023-07-23 13:10:49 +0530

    Update PayConfig.java

--- src/main/java/com/ecommerce/paytem/PayConfig.java
+++ src/main/java/com/ecommerce/paytem/PayConfig.java
@@ -3,8 +3,8 @@ package com.ecommerce.paytem;
 
 public class PayConfig {
 	
-	public static final  String MID = "smBSXG92955823926935";
-	public static final  String MKEY = "tpP9hMdiIQ0BUro4";
-	public static final  String website = "WEBSTAGING";
+	public static final  String MID = "";
+	public static final  String MKEY = "";
+	public static final  String website = "";
 	
 }
